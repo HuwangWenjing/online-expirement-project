c70ac8edf1b5894a32c20536380b0129e4745e14
HuwangWenjing <62092168+HuwangWenjing@users.noreply.github.com>
2020-12-10 21:57:09 +0800

    Update admin.py

--- job/admin.py
+++ job/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from .models import sign, teacher, student, course
 
-admin.site.register(sign)
-admin.site.register(teacher)
-admin.site.register(student)
-admin.site.register(course)
+from job.models import *
+
+class StudentAdmin(admin.ModelAdmin):
+    fields=('StuNo', 'StuName',"StuSex","Major")
+    list_display = ['StuNo', 'StuName',
+                    ]
+
+class TeacherAdmin(admin.ModelAdmin):
+    fields = ('TeaNo', 'TeaName', "TeaSex")
+    list_display = ['TeaNo', 'TeaName',
+                    'TeaSex'
+                    ]
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ('CuNo', 'CuName', "TeaNo")
+    list_display = ['CuNo', 'CuName'
+                    ]
+
+
+class NoticeAdmin(admin.ModelAdmin):
+    fields = ('NoticeTitle', 'NoticePubTime', "NoticeContent","MaNo")
+    list_display = ['NoticeNo','NoticeTitle', 'NoticeContent', 'NoticePubTime']
+
+
+
+admin.site.register(student,StudentAdmin)
+admin.site.register(teacher, TeacherAdmin)
+admin.site.register(course, CourseAdmin)
+admin.site.register(notice, NoticeAdmin)
+#admin.site.register(sign)
